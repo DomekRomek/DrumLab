32d2a56a70bfcf7dfe1d1ef6782daa0790d5edbf
DomekRomek <domekromek@tutanota.com>
2026-09-14 19:13:47 +0300

    v8.0

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ from fastapi import FastAPI, File, Form, HTTPException, UploadFile
 from fastapi.responses import FileResponse, Response
 from fastapi.staticfiles import StaticFiles
 
-APP_VERSION = "7.3"
+APP_VERSION = "8.0"
 APP_DIR = Path(__file__).resolve().parent
 WORK = APP_DIR / "workdir"
 UPLOADS = WORK / "uploads"
